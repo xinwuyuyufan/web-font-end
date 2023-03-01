924fb9a53ef02de5b2b20422ee6751f346c8b880
xinwu <xinwuyuyufan@163.com>
2023-03-01 21:55:06 +0800

    获取canvas方法

--- present/script.js
+++ present/script.js
@@ -1,3 +1,6 @@
-let canvas = document.querySelector('#canvas')
+let canvas = document.querySelector('#canvas');
+// canvas 宽高
 canvas.height = innerHeight; 
 canvas.width = innerWidth; 
+let context = canvas.getContext('2d');
+console.log(context);
