dc74c5b9fc2a3bf42a43af610793262c4d7e7cc3
xinwu <xinwuyuyufan@163.com>
2023-03-02 21:28:31 +0800

    搭建画布

--- present/script.js
+++ present/script.js
@@ -1,6 +1,20 @@
-let canvas = document.querySelector('#canvas');
-// canvas 宽高
-canvas.height = innerHeight; 
-canvas.width = innerWidth; 
-let context = canvas.getContext('2d');
-console.log(context);
+!(function () {
+  let canvas = document.querySelector("#canvas");
+  let context = canvas.getContext("2d");
+  // canvas 宽高
+  function resizeCanvas() {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    clearCanvas();
+  }
+
+  
+  function clearCanvas() {
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+  resizeCanvas();
+
+  window.addEventListener("resize", resizeCanvas);
+})();
